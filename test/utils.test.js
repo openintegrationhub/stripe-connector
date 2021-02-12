0d459d60dbf39322f9d91894d11fe8e301b4df1e
SvenHoeffler <shoeffler@wice.de>
2021-02-12 12:02:41 +0100

    Added basic structure

---
+++ test/utils.test.js
@@ -0,0 +1,10 @@
+/* eslint no-unused-expressions: "off" */
+
+const { expect } = require('chai');
+
+describe('Helpers', () => {
+  it('should placeholder test', async () => {
+    expect(true).to.be.true;
+  });
+
+});
